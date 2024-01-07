ee873a11629ac2ff1eb6f66c96eccca65ca69772
Charlotte <charlotte.kruzic@hotmail.fr>
2024-01-07 23:35:19 +0100

    Générer 100 sinus en C

--- generate_sines.py
+++ generate_sines.py
@@ -9,15 +9,15 @@ frequences = list(primerange(1009, 1722))
 phases = list(primerange(2, 542))
 
 # Fonction pour générer un sinus
-def generer_sinus(amplitude, frequence, phase):
-    return f"{amplitude}*sin({frequence}*x+6.28/{phase})"
+def generer_sinus(index, amplitude, frequence, phase):
+    return f"#define SINE{index} {amplitude}*sin({frequence}*x+6.28/{phase})"
 
 # Génération de chaque sinus
-sine_codes = [generer_sinus(amplitude, frequence, phase) for i, (amplitude, frequence, phase) in enumerate(zip(amplitudes, frequences, phases))]
+sine_codes = [generer_sinus(i+1, amplitude, frequence, phase) for i, (amplitude, frequence, phase) in enumerate(zip(amplitudes, frequences, phases))]
 
 # Affichage de la somme des sinus
-print("#define SUM =", end="\n")
+#print("#define POLY(x)", end=" ")
 print(sine_codes[0], end="\n")
 for sine_code in sine_codes[1:]:
-    print(f"+ {sine_code}", end="\n")
+    print(f"{sine_code}", end="\n")
 print()
